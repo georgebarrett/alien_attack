9ced982588ec5a92d74fac15d757008e7ff256af
geab <georgeaustinbarrett@gmail.com>
2024-11-01 18:48:57 +0000

    more robust test written for class Finished. print statement changed in class. test running GREEN

--- alien_attack/scenes/finished.py
+++ alien_attack/scenes/finished.py
@@ -3,7 +3,5 @@ from alien_attack.scenes.base_scene import Scene
 class Finished(Scene):
     
     def enter(self):
-        return {
-            "scene": "finished",
-            "message": "You made it!"
-        }
+        print('You survived!')
+        return 'finished'
--- tests/scenes/test_finished.py
+++ tests/scenes/test_finished.py
@@ -1,7 +1,13 @@
+import pytest
 from alien_attack.scenes.finished import Finished
 
-def test_finished_scene():
-    scene = Finished()
-    result = scene.enter()
-    assert result["scene"] == "finished"
-    assert result["message"] == "You made it!"
+@pytest.fixture
+def finished_scene():
+    return Finished()
+
+def test_finished_enter_output(finished_scene, capsys):
+    result = finished_scene.enter()
+    captured = capsys.readouterr()
+
+    assert "You survived!" in captured.out, "Output should contain 'You survived!'"
+    assert result == 'finished', "Return value should be 'finished'"
